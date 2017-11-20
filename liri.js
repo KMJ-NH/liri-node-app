17445901d7b6615133ea152a65ff53e64b615475
kmj-nh <sh-kj@msn.com>
2017-11-20 13:29:20 -0500

    These are all of the initial files for the Node homework

---
+++ liri.js
@@ -0,0 +1,42 @@
+//Here there will be code written to grab the data from keys.js. The keys will be stored in a variable.
+
+//This file will be able to take in one of the following commands:
+  //my-tweets
+  //spotify-this-song
+  //movie-this
+  //do-what-it-says
+
+
+  // TWITTER SECTION
+  //The command should do this -- node liri.js my-tweets  -- which would show that last 20 tweets and when they were created.
+
+
+  // SPOTIFY SECTION
+  //The command would also do -- node liri.js spotify-this-song '<song name here>'
+    //This would display in the terminal window:
+      // Artist(s)
+      // The song's name
+      // A preview link of the song from Spotify
+      // The album that the song is from
+      // If no song is provided then the program will default to "The Sign" by Ace of Base.
+
+
+  // OMDB SECTION
+  //The command would also do -- node liri.js movie-this '<movie name here>'
+    //This would display in the terminal window:
+      //  Title of the movie
+      //  Year the movie came out
+      //  IMDB rating of the movie
+      //  Rotten Tomatoes rating of the movie
+      //  Country where the movie was produced
+      //  Language of the movie
+      //  Plot of the movie
+      //  Actors in the movie
+      //  If no movie is typed in, data for the moive 'Mr. Nobody' will be displayed
+
+    // The fs Node package will be used for LIRI to use the text inside random.txt to call one of LIRI's commands
+      // This means it would run spotify-this-song for "I Want it That Way," as the random.txt file contained that text  
+
+  
+
+
